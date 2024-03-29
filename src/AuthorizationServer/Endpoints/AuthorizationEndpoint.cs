4a1776c1744facd1c2d92d98d22ac413e5515082
Leandro G <leandru@gmail.com>
2024-03-29 13:02:50 -0300

    feat: implementing client credential flow endpoint

---
+++ src/AuthorizationServer/Endpoints/AuthorizationEndpoint.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.IdentityModel.Tokens;
+using OpenIddict.Abstractions;
+using OpenIddict.Server.AspNetCore;
+using System.Security.Claims;
+using static OpenIddict.Abstractions.OpenIddictConstants;
+
+namespace AuthorizationServer.Endpoints;
+
+public static class AuthorizationEndpoints
+{
+    public static WebApplication MapAuthorizationEndpoints(this WebApplication app)
+    {
+        app.MapPost("/connect/token", Exchange).WithOpenApi();
+        return app;
+    }
+
+    public static IResult Exchange(HttpContext httpContext)
+    {
+        OpenIddictRequest? request = httpContext.GetOpenIddictServerRequest() ??
+            throw new InvalidOperationException("The OpenID Connect request cannot be retrieved.");
+
+        if (request.IsClientCredentialsGrantType())
+        {
+            string? clientId = request.ClientId;
+            ClaimsIdentity identity = new(authenticationType: TokenValidationParameters.DefaultAuthenticationType);
+            identity.SetClaim(Claims.Subject, clientId);
+            identity.SetScopes(request.GetScopes());
+            ClaimsPrincipal principal = new(identity);
+
+            return Results.SignIn(principal, properties: null, OpenIddictServerAspNetCoreDefaults.AuthenticationScheme);
+        }
+
+        throw new NotImplementedException("The specified grant type is not implemented.");
+    }
+}
--- src/AuthorizationServer/Program.cs
+++ src/AuthorizationServer/Program.cs
@@ -1,3 +1,4 @@
+using AuthorizationServer.Endpoints;
 using AuthorizationServer.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,29 +19,7 @@ var app = builder.Build();
 
     app.UseHttpsRedirection();
 
-    var summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-            new WeatherForecast
-            (
-                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                Random.Shared.Next(-20, 55),
-                summaries[Random.Shared.Next(summaries.Length)]
-            ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+    app.MapAuthorizationEndpoints();
 
     app.Run();
-}
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
